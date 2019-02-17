e7742bb787dced4953e90e63e4c0dd04ee1bd3f7
ksuedile <38907081+ksuedile@users.noreply.github.com>
2019-02-17 16:56:29 +0100

    Update player.ts

--- src/Player/player.ts
+++ src/Player/player.ts
@@ -20,7 +20,7 @@ export class Player implements IMapObj{
                 public vector : BABYLON.Vector3) {
         KeyMapSingleton.getInstance().add(KeyMapEvent.KEY_DOWN, (event)=>{
 
-            let forward = new BABYLON.Vector3(0,9.81*1.5, 0.1);
+            let forward = new BABYLON.Vector3(0,0.5, 0.1);
             forward = forward.negate();
             this.meshes[0].moveWithCollisions(forward);
             console.log('==>',event.keyCode == 90 )
